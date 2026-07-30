9c3c3127f19ce7ce0aef816e43387a0f750287d1
jancely <49329507+jancely@users.noreply.github.com>
2026-07-30 19:22:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,15 +13,18 @@ parser = argparse.ArgumentParser(description='[Adaboost] Adaboost regression for
 
 parser.add_argument('--model', type=str, required=True, default='Adaboost', help='model name')
 parser.add_argument('--root_path', type=str, required=True, default='./raw_data/',  help='root path of the data file')
-parser.add_argument('--data_path', type=str, required=True, default='N2OALL.csv', help='data file')
-parser.add_argument('--random_state', type=int, required=True, default=100, help='random state of adaboost regressor')
-parser.add_argument('--target', type=str, required=True, default=['lnRR', 'N2O'], help='target prediction columns')
+parser.add_argument('--data_path', type=str, required=True, default='SOCALL_New.xlsx', help='data file')
+parser.add_argument('--repeat', type=int, required=False, default=10, help='epochs of adaboost regressor')
+parser.add_argument('--random_state', type=int, required=False, default=2000, help='epochs of adaboost regressor')
+parser.add_argument('--krandom_state', type=int, required=False, default=100, help='epochs of adaboost regressor')
+parser.add_argument('--seed', type=int, required=False, default=2026, help='random seed of adaboost regressor')  
+parser.add_argument('--target', type=str, required=True, default=['SOCT', 'SOCC'], help='target prediction columns')
 parser.add_argument('--do_predict', type=str, required=True, default=True, help='predict global fertilizer substance')
 
 parser.add_argument('--checkpoints', type=str, default='./checkpoints/', help='location of model checkpoints')
 parser.add_argument('--output', type=str, default='./prediction/', help='prediction save path')
 
-parser.add_argument('--redudent', type=str, default=['Crop1', 'Crop2', 'Family', 'LE', 'Cropcombination'],
+parser.add_argument('--redudent', type=str, default=['Planting', 'Family', 'LE', 'lnRR'],
                     help='input sequence length of Informer encoder')
 parser.add_argument('--cru_file', type=str, default='./global_data/MAT/cru_ts4.06.2021.2021.tmp.dat.nc', help='cru data')
 parser.add_argument('--gpcc_file', type=str, default='.\global_data\MAP\\normals_1991_2020_v2022_05.nc', help='gpcc data')
@@ -32,6 +35,7 @@ parser.add_argument('--bd_file', type=str, default='.\global_data\T_BULK_DEN.nc4
 parser.add_argument('--soc_file', type=str, default='.\global_data\T_OC.nc4', help='soc data')
 parser.add_argument('--ph_file', type=str, default='.\global_data\T_PH_H2O.nc4', help='ph data')
 parser.add_argument('--crop_file', type=str, default='G:\Project_Code\Adaboost_Regression\Cropcombination.xlsx', help='crop data')
+parser.add_argument('--fertizer_file', type=str, default='.\global_data\\nmanure_app_crop.nc', help='fertilizer data')
 parser.add_argument('--land_file', type=str, default='.\global_data\MCD12C1.A2021001.061.2022217040006.hdf', help='land data')
 
 parser.add_argument('--use_gpu', type=bool, default=True, help='use gpu')
@@ -52,92 +56,86 @@ if args.use_gpu and args.use_multi_gpu:
 
 print('Args in experiment:')
 
-target = args.target[1]
+target = args.target[-1]
 setting = '{}_{}'.format(args.model, target)
 
 Exp = Exp_Adaboost
 
-R2trainall = []
-R2testall = []
-# Prediction_lnRR = []
-# Prediction_N2O = []
-Weights = []
-
-judge = 0
 checkpoint_path = os.path.join(args.checkpoints, setting)
 if not os.path.exists(checkpoint_path):
     os.makedirs(checkpoint_path)
 
-if target[1] == 'SOC':
-    seed = 78150
-elif target[1] == 'NL':
-    seed = 1025
-elif target[1] == 'CO2':
-    seed = 700
-else:
-    seed = 29
-
-for i in range(args.random_state):
-    args.seed = seed
-    args.material = args.target[1]
-    exp = Exp(args)  # set experiments
-    print('>>>>>>>start training : {}>>>>>>>>>>>>>>>>>>>>>>>>>>'.format(setting))
-    R2train, R2test, weigth, model = exp.train(setting)
-    if judge >= R2test:
-        pass
-    else:
-        best_model = model
-        best_model_path = checkpoint_path + '/' + 'checkpoint.pth'
-        with open(best_model_path, 'wb') as f:
-            cPickle.dump(model, f)
-
-        judge = R2test
-    # R2train, R2test= exp.train(setting)
-
-    R2trainall.append(R2train)
-    R2testall.append(R2test)
-    Weights.append(weigth)
-
-#
-mean_trainR2 = np.round(np.mean(R2trainall), 4)
-mean_train_std = np.round(np.std(R2trainall), 3)
-mean_testR2 = np.round(np.mean(R2testall), 4)
-mean_test_std = np.round(np.std(R2testall), 3)
-
-print("Global| Train R2: {0:.7f} +/- {1: .3f} Test R2: {2:.7f} +/- {3: .3f}".format(mean_trainR2, mean_train_std,
-                                                                                    mean_testR2, mean_test_std))
-
-
-if args.do_predict:
-    print('\n>>>>>>>predicting : {}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'.format(setting))
-    Exp_Pre = Exp_Predict(args)
-
-    lnRR, N2O = Exp_Pre.predict(best_model)
-    # Prediction_lnRR.append(lnRR)
-    # Prediction_N2O.append(N2O)
-
+print('>>>>>>>start training : {}>>>>>>>>>>>>>>>>>>>>>>>>>>'.format(setting))
+seed = args.seed
+args.material = args.target[0]
+exp = Exp(args)  # set experiments
+r2, models, model = exp.train(setting)
 
 # result save
 folder_path = './results/' + setting + '/'
 if not os.path.exists(folder_path):
     os.makedirs(folder_path)
 
-# Prediction_lnRR = np.mean(Prediction_lnRR, axis=0)
-# Prediction_N2O = np.mean(Prediction_N2O, axis=0)
+print('\n>>>>>>>Bootstrap predicting : {}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'.format(setting))
+if args.do_predict:
+    Exp_Pre = Exp_Predict(args)
+    # pred, Lo, La, landcover = Exp_Pre.predict(model)
+    N2OT, N2OC, Lo, La, landcover = Exp_Pre.predict(model)
+    lnRR = np.log(N2OT / N2OC)
+    Lo = Lo.reshape(360, 720)
+    La = La.reshape(360, 720)
+    lnRR = lnRR.reshape(360, 720)
+    landcover = landcover.reshape(360, 720)
+    lnRR[landcover == 0] = np.nan
+    lnRR = pd.DataFrame(lnRR)
+    writer1 = pd.ExcelWriter('./Global_Prediction/Tables/final/PredGlobal_{}_lnRR.xlsx'.format(target), engine='xlsxwriter')
+    lnRR.to_excel(writer1, float_format='%.4f')
+    writer1.close()
 
-lnRR_N2O = pd.DataFrame(lnRR)
-pred_N2O = pd.DataFrame(N2O)
-weights = pd.DataFrame(Weights)
-# pred.columns = ['lnRR', 'N2O']
-writer1 = pd.ExcelWriter(folder_path + str(args.target[1]) + '_lnRR.xlsx')
-writer2 = pd.ExcelWriter(folder_path + str(args.target[1]) + '.xlsx')
-writer3 = pd.ExcelWriter(folder_path + str(args.target[1]) + '_weight.xlsx')
-lnRR_N2O.to_excel(writer1, float_format='%.4f')
-pred_N2O.to_excel(writer2, float_format='%.4f')
-weights.to_excel(writer3, float_format='%.4f')
-writer1._save()
-writer2._save()
-writer3._save()
+# Bootstrap prediction
+    lnRR_Boots = []
+    for md in models:
+        N2OT, N2OC, Lo, La, landcover = Exp_Pre.predict(md)
+        lnRR = np.log(N2OT / N2OC)
+        lnRR_Boots.append(lnRR)
+        lnRR = N2OC.reshape(360, 720)
+        landcover = landcover.reshape(360, 720)
+        lnRR[landcover == 0] = np.nan
 
+    lnRR_Boots = np.array(lnRR_Boots).T
+    std_lnRR = lnRR_Boots.std(axis=1)
+    median_lnRR = np.median(lnRR_Boots, axis=1)
+    
+    # Coefficient of Variation
+    cv_lnRR = (std_lnRR / (np.abs(median_lnRR) + 1e-10))
+    Lo = Lo.reshape(360, 720)
+    La = La.reshape(360, 720)
+    landcover = landcover.reshape(360, 720)
+    cv_lnRR_map = cv_lnRR.reshape(360, 720)
+    cv_lnRR_map[landcover == 0] = np.nan
+    lnRR_cv = pd.DataFrame(cv_lnRR_map)
+    writer2 = pd.ExcelWriter('./Global_Prediction/Tables/final/Bootstrap_{}_cv.xlsx'.format(target), engine='xlsxwriter')
+    lnRR_cv.to_excel(writer3, float_format='%.4f')
+    writer2.close()
+
+# result save
+folder_path = './results/' + setting + '/'
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+
+if args.do_predict:
+    print('\n>>>>>>>predicting : {}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'.format(setting))
+    Exp_Pre = Exp_Predict(args)
+
+    N2OT, N2OC, Lo, La, landcover = Exp_Pre.predict(model)
+    lnRR = np.log(N2OT / N2OC)
+    lnRR[landcover.ravel() == 0] = np.nan
+
+    lnRR = pd.DataFrame(lnRR)
+    writer3 = pd.ExcelWriter(folder_path + str(args.target[1]) + '{}_lnRR.xlsx'.format(target), engine='xlsxwriter')
+    lnRR.to_excel(writer3, float_format='%.4f')
+    writer3.close()
+
+print('File writen down')
 
 
